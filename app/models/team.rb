d1a73acea0f548bab7818135238dd5282d25b19c
Pedro Pimentel <zukunft@gmail.com>
2010-06-24 23:56:06 -0300

    create has_many relation: Team has many matches using custom finder

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,5 +4,8 @@ class Team < ActiveRecord::Base
 
   #has_many :matches_as_home_team, :class_name => "Match", :foreign_key => "home_team_id"
   #has_many :matches_as_away_team, :class_name => "Match", :foreign_key => "away_team_id"
+
+  has_many :matches, :class_name => "Match",
+    :finder_sql => 'SELECT * FROM matches WHERE (matches.home_team_id = #{id} OR matches.away_team_id = #{id})'
 end
 
