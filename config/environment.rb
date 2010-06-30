c42f5b5482c201060c2015a3cf5fdffbf3962ea6
marano <thiagomarano@gmail.com>
2010-06-30 13:13:52 -0300

    Fixed dependencies.

--- config/environment.rb
+++ config/environment.rb
@@ -28,8 +28,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'builder'
-  config.gem 'xml-simple'
-  config.gem 'sqlite3-ruby'
+  #config.gem 'xml-simple'
+  #config.gem 'sqlite3-ruby'
 
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
